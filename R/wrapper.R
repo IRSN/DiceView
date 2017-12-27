28035511e87060f939503f7a4b6405d3298bc82d
Yann <yann.richet@laposte.net>
2017-12-27 17:15:34 +0100

    sync with updated CRAN policy.

--- R/wrapper.R
+++ R/wrapper.R
@@ -24,7 +24,7 @@ if(!isGeneric("view")) {
                )
 }
 
-setMethod("view", c("character","km"), 
+setMethod("view", c("character","km"),
           function(type = "auto",
                    model, model.type = "UK",
                    center = NULL, axis = NULL,
@@ -45,7 +45,7 @@ setMethod("view", c("character","km"),
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               if(is.null(type)) type="auto"
               if(type=="auto"){
                   if(model@d==1) type="xy"
@@ -55,7 +55,7 @@ setMethod("view", c("character","km"),
                       if(is.null(center)) center=colMeans(model@X)
                   }
               }
-              
+
               if (type == "section" || type == "xy")
                   sectionview.km(model = model, type = model.type,
                                  center = center, axis = axis,
@@ -74,7 +74,7 @@ setMethod("view", c("character","km"),
                                  ylim = ylim,
                                  title = title,
                                  ...)
-              
+
               if (type == "section3d" || type == "3d")
                   sectionview3d.km(model = model, type = model.type,
                                    center = center, axis = axis,
@@ -93,7 +93,7 @@ setMethod("view", c("character","km"),
                                    ylim = ylim,
                                    title = title,
                                    ...)
-              
+
               if (type == "contour")
                   contourview.km(model = model, type = model.type,
                                  center = center, axis = axis,
@@ -114,7 +114,7 @@ setMethod("view", c("character","km"),
           }
           )
 
-setMethod("view", c("character","list"), 
+setMethod("view", c("character","list"),
           function(type = "auto",
                    model,
                    center = NULL, axis = NULL,
@@ -133,7 +133,7 @@ setMethod("view", c("character","list"),
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               if(is.null(type)) type="auto"
               if(type=="auto"){
                   if(ncol(model$data$X)==1) type="xy"
@@ -143,7 +143,7 @@ setMethod("view", c("character","list"),
                       if(is.null(center)) center=colMeans(model$data$X)
                   }
               }
-              
+
               if (type == "section" || type == "xy")
                   sectionview.list(model = model,
                                    center = center, axis = axis,
@@ -160,7 +160,7 @@ setMethod("view", c("character","list"),
                                    ylim = ylim,
                                    title = title,
                                    ...)
-              
+
               if (type == "section3d" || type == "3d")
                   sectionview3d.list(model = model,
                                      center = center, axis = axis,
@@ -177,7 +177,7 @@ setMethod("view", c("character","list"),
                                      ylim = ylim,
                                      title = title,
                                      ...)
-              
+
               if (type == "contour")
                   contourview.list(model = model,
                                    center = center, axis = axis,
@@ -198,7 +198,7 @@ setMethod("view", c("character","list"),
           }
           )
 
-setMethod("view", c("character","function"), 
+setMethod("view", c("character","function"),
           function(type = "auto",
                    model , dim,
                    center = NULL, axis = NULL,
@@ -214,7 +214,7 @@ setMethod("view", c("character","function"),
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               if(is.null(type)) type="auto"
               if(type=="auto"){
                   if(dim==1) type="xy"
@@ -224,12 +224,12 @@ setMethod("view", c("character","function"),
                       if(is.null(center)) if(!is.null(xlim)) center=colMeans(xlim) else center=rep(0.5,dim)
                   }
               }
-              
+
               if (type == "section" || type == "xy")
                   sectionview.fun(fun = model, dim = dim,
                                   center = center, axis = axis,
                                   npoints = ifelse(is.null(npoints),100,npoints),
-                                  col = col,
+                                  col_surf = col,
                                   mfrow = mfrow,
                                   Xname = Xname,
                                   yname=yname,
@@ -239,7 +239,7 @@ setMethod("view", c("character","function"),
                                   ylim = ylim,
                                   title = title,
                                   ...)
-              
+
               if (type == "section3d" || type == "3d")
                   sectionview3d.fun(fun = model, dim = dim,
                                     center = center, axis = axis,
@@ -253,7 +253,7 @@ setMethod("view", c("character","function"),
                                     ylim = ylim,
                                     title = title,
                                     ...)
-              
+
               if (type == "contour")
                   contourview.fun(fun = model, dim = dim,
                                   center = center, axis = axis,
@@ -280,7 +280,7 @@ if(!isGeneric("sectionview")) {
                )
 }
 
-setMethod("sectionview", "km", 
+setMethod("sectionview", "km",
           function(model,
                    type = "UK",
                    center = NULL,
@@ -299,7 +299,7 @@ setMethod("sectionview", "km",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               sectionview.km(model = model,
                              type = type,
                              center = center,
@@ -317,11 +317,11 @@ setMethod("sectionview", "km",
                              xlim = xlim,
                              ylim = ylim,
                              title = title,
-                             ...)		
+                             ...)
           }
           )
 
-setMethod("sectionview", "list", 
+setMethod("sectionview", "list",
           function(model,
                    center = NULL,
                    npoints = 100,
@@ -337,7 +337,7 @@ setMethod("sectionview", "list",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               sectionview.list(model = model,
                                center = center,
                                npoints = npoints,
@@ -352,11 +352,11 @@ setMethod("sectionview", "list",
                                xlim = xlim,
                                ylim = ylim,
                                title = title,
-                               ...)		
+                               ...)
           }
           )
 
-setMethod("sectionview", "function", 
+setMethod("sectionview", "function",
           function(model,dim,
                    center = NULL, axis = NULL,
                    npoints = 100,
@@ -370,12 +370,12 @@ setMethod("sectionview", "function",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               sectionview.fun(fun = model,
                               dim = dim,
                               center = center, axis = axis,
                               npoints = npoints,
-                              col = col,
+                              col_surf = col,
                               mfrow = mfrow,
                               Xname = Xname,
                               yname = yname,
@@ -384,7 +384,7 @@ setMethod("sectionview", "function",
                               xlim = xlim,
                               ylim = ylim,
                               title = title,
-                              ...)        
+                              ...)
           }
           )
 
@@ -395,7 +395,7 @@ if(!isGeneric("sectionview3d")) {
                )
 }
 
-setMethod("sectionview3d", "km", 
+setMethod("sectionview3d", "km",
           function(model, type = "UK",
                    center = NULL, axis = NULL,
                    npoints = 20,
@@ -413,7 +413,7 @@ setMethod("sectionview3d", "km",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               sectionview3d.km(model = model, type = type,
                                center = center, axis = axis,
                                npoints = npoints,
@@ -430,11 +430,11 @@ setMethod("sectionview3d", "km",
                                xlim = xlim,
                                ylim = ylim,
                                title = title,
-                               ...)		
+                               ...)
           }
           )
 
-setMethod("sectionview3d", "list", 
+setMethod("sectionview3d", "list",
           function(model,
                    center = NULL, axis = NULL,
                    npoints = 20,
@@ -449,7 +449,7 @@ setMethod("sectionview3d", "list",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               sectionview3d.list(model = model,
                                  center = center, axis = axis,
                                  npoints = npoints,
@@ -463,11 +463,11 @@ setMethod("sectionview3d", "list",
                                  xlim = xlim,
                                  ylim = ylim,
                                  title = title,
-                                 ...)		
+                                 ...)
           }
           )
 
-setMethod("sectionview3d", "function", 
+setMethod("sectionview3d", "function",
           function(model,dim,
                    center = NULL, axis = NULL,
                    npoints = 20,
@@ -480,7 +480,7 @@ setMethod("sectionview3d", "function",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               sectionview3d.fun(fun = model, dim = dim,
                                 center = center, axis = axis,
                                 npoints = npoints,
@@ -492,7 +492,7 @@ setMethod("sectionview3d", "function",
                                 xlim = xlim,
                                 ylim = ylim,
                                 title = title,
-                                ...)        
+                                ...)
           }
           )
 
@@ -503,7 +503,7 @@ if(!isGeneric("contourview")) {
                )
 }
 
-setMethod("contourview", "km", 
+setMethod("contourview", "km",
           function(model, type = "UK",
                    center = NULL, axis = NULL,
                    npoints = 20,
@@ -519,7 +519,7 @@ setMethod("contourview", "km",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               contourview.km(model = model, type = type,
                              center = center, axis = axis,
                              npoints = npoints,
@@ -534,11 +534,11 @@ setMethod("contourview", "km",
                              xlim = xlim,
                              ylim = ylim,
                              title = title,
-                             ...)        
+                             ...)
           }
           )
 
-setMethod("contourview", "list", 
+setMethod("contourview", "list",
           function(model,
                    center = NULL, axis = NULL,
                    npoints = 20,
@@ -554,7 +554,7 @@ setMethod("contourview", "list",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               contourview.list(model = model,
                                center = center, axis = axis,
                                npoints = npoints,
@@ -569,11 +569,11 @@ setMethod("contourview", "list",
                                xlim = xlim,
                                ylim = ylim,
                                title = title,
-                               ...)        
+                               ...)
           }
           )
 
-setMethod("contourview", "function", 
+setMethod("contourview", "function",
           function(model,dim,
                    center = NULL, axis = NULL,
                    npoints = 20,
@@ -587,7 +587,7 @@ setMethod("contourview", "function",
                    ylim = NULL,
                    title = NULL,
                    ...){
-              
+
               contourview.fun(fun = model, dim = dim,
                               center = center, axis = axis,
                               npoints = npoints,
@@ -600,6 +600,6 @@ setMethod("contourview", "function",
                               xlim = xlim,
                               ylim = ylim,
                               title = title,
-                              ...)        
+                              ...)
           }
           )
