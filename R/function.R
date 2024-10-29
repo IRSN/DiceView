792c48cb3fdb5522acb2019f3f23dc429e8c780e
yannrichet <richetyann@gmail.com>
2024-10-29 16:55:56 +0100

    add generalized expand.grids and eval intervalfunctions

--- R/function.R
+++ R/function.R
@@ -87,3 +87,87 @@ Memoize.function <- function(fun) {
         res
     }
 }
+
+#' Create a Data Frame from all combinations of factor variables
+#' @description Generalization of base::expand.grid to more than 2 variables.
+#' @param d number of variables (taken in following arguments with modulo)
+#' @param ... variables to combine, as arrays of values
+#' @return data frame of all possible combinations of variables values
+#' @tests
+#' expand.grids(d=1)
+#' expand.grids(d=1,seq(f=0,t=1,l=11))
+#' expand.grids(d=2)
+#' expand.grids(d=2,seq(f=0,t=1,l=11))
+#' expand.grids(d=2,seq(f=0,t=1,l=11),seq(0,1,l=3))
+#' expand.grids(d=3,seq(f=0,t=1,l=5))
+#' expand.grids(d=NULL,seq(f=0,t=1,l=5),seq(f=0,t=1,l=5),seq(f=0,t=1,l=5))
+#' expand.grids(seq(f=0,t=1,l=5),seq(f=0,t=1,l=5),seq(f=0,t=1,l=5))
+#' expand.grids(d=4,seq(f=0,t=1,l=5))
+expand.grids <- function(d=length(list(...)),...) {
+    if (isTRUE(is.na(d)) || isTRUE(is.null(d))) d = length(list(...)) # when d is explicitely NA, compute it.
+    s = list(...)
+    if (length(d)>0) { # When d is not set, it effectively contains first sample.
+        s = c("d",s)
+        s[[1]] = d
+        d = length(s)
+    }
+    if (length(s)==0) s=list(c(0,1)) # when no ... provided, uses {0,1}
+    if (length(s)==1 & is.list(s[[1]])) s = s[[1]] # when ... is already a list of samples
+    l=list(s[[1]])
+    if (d>1) for (i in 2:d) {
+        l=c(l,list(s[[ (i-1) %% length(s) + 1 ]])) # weird formula when table index starts from 1...
+    }
+    return(do.call("expand.grid",l))
+}
+
+#' eval function and cast result to a list of y, y_low, y_up (possibly NA)
+#' @param fun function to evaluate
+#' @param X matrix of input values for fun
+#' @param vectorized whether fun is vectorized or not
+#' @param dim dimension of input values for fun if
+#' @return list of y, y_low, y_up
+EvalInterval.function = function(fun,X, vectorized=FALSE, dim=ncol(X)) {
+        if (!vectorized)
+            Fun = Vectorize.function(fun, dim)
+        else
+            Fun = fun
+
+        if (is.null(X)) {
+            if (is.null(dim)) dim = 1
+            X = matrix(c(0,1), ncol=dim, nrow=2)
+        }
+
+        f_x <- Fun(as.matrix(X))
+
+        # Now cast that eval :
+        y <- NA #array(NA, npoints)
+        y_low <- NA #array(NA, npoints)
+        y_up <- NA #array(NA, npoints)
+        if (is.list(f_x)) {
+            f_x = lapply(as.list(as.data.frame(f_x)),unlist)
+            if (!("mean" %in% names(f_x)) || !("se" %in% names(f_x)))
+                stop(paste0("If function returns a list, it must have 'mean' and 'se', while was ",paste0(collapse="\n",utils::capture.output(print(f_x)))))
+            y <- as.numeric(f_x$mean)
+            if (!is.numeric(y))
+                stop("If function returns a list, 'mean' must be (as) numeric:",paste0(y,collapse="\n"))
+
+            if (!is.numeric(as.numeric(f_x$se)))
+                stop("If function returns a list, 'se' must be (as) numeric:",paste0(f_x$se,collapse="\n"))
+            y_low <- y - as.numeric(f_x$se)
+            y_up <- y + as.numeric(f_x$se)
+
+        } else if (is.matrix(f_x) && ncol(f_x)==2) {
+            y_low <- as.numeric(f_x[,1])
+            y_up <- as.numeric(f_x[,2])
+            if (!is.numeric(y_low))
+                stop("If function returns a matrix, first column must be (as) numeric.")
+            if (!is.numeric(y_up))
+                stop("If function returns a matrix, second column must be (as) numeric.")
+        } else { # simple function, not a list nor a matrix
+            y <- as.numeric(f_x)
+            if (!is.numeric(y))
+                stop("If function does not returns a list, it must be (as) numeric.")
+        }
+
+        return(list(y=y,y_low=y_low,y_up=y_up))
+}
